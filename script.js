a6022f772d78291e04771f36d5cf5032540f135c
Chen, Danny <danny50610@yahoo.com.tw>
2024-11-19 20:35:58 +0800

    WIP call ai model

---
+++ script.js
@@ -0,0 +1,47 @@
+(async () => {
+    const errorMessage = document.getElementById("error-message");
+    const passwordRulesInput = document.getElementById("password-rules-input");
+    const generateButton = document.getElementById("generate");
+
+    if (!self.ai || !self.ai.languageModel) {
+        errorMessage.style.display = "block";
+        errorMessage.innerHTML = `Your browser doesn't support the Prompt API. If you're on Chrome, join the <a href="https://developer.chrome.com/docs/ai/built-in#get_an_early_preview">Early Preview Program</a> to enable it.`;
+        return;
+    }
+
+    const capabilities = await ai.languageModel.capabilities();
+    console.log(capabilities);
+
+    // TEST
+    passwordRulesInput.value = 'At least 10 characters, must include a mix of letters and numbers, with no more than 4 consecutive numeric characters';
+
+    generateButton.addEventListener("click", async () => {
+        const prompt = passwordRulesInput.value.trim();
+
+        const session = await self.ai.languageModel.create({
+            temperature: 0.7,
+            topK: 3,
+            systemPrompt: `
+Think step by step, and when you finally output the result, put it on a separate line and add: "Output: " at the beginning.
+You are an office assistant responsible for helping your supervisor generate passwords that meet company requirements.
+
+If the company's password complexity does not have a minimum password length limit, the default is a minimum of 8 characters
+If the company's password complexity does not have a maximum password length limit, the default maximum number of characters requires twice the minimum number of characters.
+
+            `,
+        });
+
+        const result = await session.prompt(`
+The following are the company’s password complexity requirements:
+\`\`\`
+${prompt}
+\`\`\`
+
+Regarding this requirement, what is the maximum number of characters in the password?
+        `);
+
+        console.log(result);
+
+        session.destroy();
+    });
+})();
